b57e07ab5a04cc9cb6b97e54acae08b50041c87b
Joe Quattrone <joequattrone5@gmail.com>
2019-05-29 08:36:08 -0400

    first

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import reducer from './reducers/index'
 
 it('renders without crashing', () => {
+  const store = createStore(reducer, applyMiddleware(thunk))
+
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />, div
+    </Provider>
+  );
   ReactDOM.unmountComponentAtNode(div);
 });
+
+
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <Navbar />
+//     <App />
+//   </Provider>,
+//   document.getElementById('root')
+// );
