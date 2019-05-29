05eea7a7e8e43e0c88fb8f6f5b1e811da9a7549d
Joe Quattrone <joequattrone5@gmail.com>
2019-05-29 11:14:35 -0400

    update app.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,32 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import reducer from './reducers/index'
+import Navbar from './components/Navbar'
 
-// it('renders without crashing', () => {
-//   const store = createStore(reducer, applyMiddleware(thunk))
-//
-//   const div = document.createElement('div');
-//   ReactDOM.render(
-//     <Provider store={store}>
-//       <App />, div
-//     </Provider>
-//   );
-//   ReactDOM.unmountComponentAtNode(div);
-// });
+
+it('renders without crashing', () => {
+  const store = createStore(reducer, applyMiddleware(thunk))
+  const div = document.createElement('div');
+
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    div
+  );
+});
 
 it('expects true to equal true', () => {
   expect(true).toBe(true)
 })
-
-
-// ReactDOM.render(
-//   <Provider store={store}>
-//     <Navbar />
-//     <App />
-//   </Provider>,
-//   document.getElementById('root')
-// );
