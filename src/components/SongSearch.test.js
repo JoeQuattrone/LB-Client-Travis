dd9986434e9cdbe59131801a7fc6b787fa040526
Joe Quattrone <joequattrone5@gmail.com>
2019-05-30 16:55:41 -0400

    write songs for songSearch

--- src/components/SongSearch.test.js
+++ src/components/SongSearch.test.js
@@ -2,6 +2,8 @@ import React from 'react'
 import Enzyme, { shallow, mount } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import SongSearch from './SongSearch'
+import sinon from "sinon";
+
 
 Enzyme.configure({ adapter: new Adapter() })
 
@@ -10,20 +12,7 @@ describe('<SongSearch />', () => {
     const wrapper = shallow(<SongSearch />)
     expect(wrapper.state().songTitle).toEqual('')
   })
-})
 
-  // Enzyme does not support event propigation
-  // it('should pass form input to state.songTitle', () => {
-  //   const searchSongs = jest.fn()
-  //   const wrapper = shallow(<SongSearch searchSongs={searchSongs}/>)
-  //     wrapper.find('form')
-  //     .simulate('submit', { target: { value: "foo" }})
-  //
-  //   expect(wrapper.props().handleSubmit).toHaveBeenCalled()
-  //   expect(wrapper.props().searchSongs).toHaveBeenCalled()
-  // })
-
-describe('<SongSearch />', () => {
   it("should save the songTitle in state when the input changes", () => {
     const wrapper = shallow(<SongSearch />)
 
@@ -33,4 +22,14 @@ describe('<SongSearch />', () => {
     });
     expect(wrapper.state().songTitle).toBe("Lose Yourself");
   });
-})
+
+  it("should call the `searchSongs` callback prop when the form is being submitted", () => {
+      let spy = sinon.spy()
+      const wrapper = shallow(<SongSearch searchSongs={spy} />);
+      wrapper.find("#home-searchbar").simulate("change", {
+        target: { name: "songTitle", id: "#home-searchbar", value: "Lose Yourself" }
+      });
+      wrapper.find("form").simulate("submit", { preventDefault: () => {} });
+      expect(spy.called).toBe(true)
+    });
+  });
