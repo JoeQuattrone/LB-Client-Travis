5ce08f52f3348b95de57b35b57b6502ad04965d1
Joe Quattrone <joequattrone5@gmail.com>
2019-05-29 20:07:25 -0400

    uncomplete test for ASS_SONGS

--- src/actions/songActions.test.js
+++ src/actions/songActions.test.js
@@ -2,8 +2,6 @@ import configureMockStore from 'redux-mock-store'
 import thunk from 'redux-thunk'
 import * as actions from './songActions'
 import expect, { createSpy, spyOn, isSpy } from 'expect'
-// import nock from 'nock'
-// import fetch from 'isomorphic-fetch';
 import fetchMock from 'fetch-mock'
 
 // change to redux thunk
@@ -12,39 +10,50 @@ const mockStore = configureMockStore(middlewares)
 const BASE_URL = "https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/"
 
 
-// describe('fetchSongs action', () => {
-//   afterEach(() => {
-//     nock.cleanAll()
-//   })
-//
-//   const state = {songTitle: "Lose Yourself"}
-//
-//   it('uses redux-promise to create an action object with type of "ADD_SONG" and a payload of songs when fetchSongs is dispatched', () => {
-//     const mockSongQuery = `track.search?q_track=${state.songTitle}&page_size=10&page=1&s_track_rating=desc&apikey=523ebe747e1a258aaddd09f97f90cb70`
-//
-//     nock(BASE_URL)
-//       .get(mockSongQuery)
-//       .reply(200,
-//         {
-//           trackList: [ {track: {artist_name: "Eminem", track_name: "Lose Yourself"}}, {track: {artist_name: "Daft Punk feat. Pharrell Williams", track_name: "Lose Yourself to Dance"}} ]
-//         })
-//     })
-//
-//     const expectedActions = [
-//       {type: 'LOADING_SONG'},
-//       { type: "ADD_SONG", payload: [
-//         {track: {artist_name: "Eminem", track_name: "Lose Yourself"}}, {track: {artist_name: "Daft Punk feat. Pharrell Williams", track_name: "Lose Yourself to Dance"}}
-//       ] }
-//     ]
-//
-//     const store = mockStore({})
-//     global.fetch = fetch
-//
-//     return store.dispatch(actions.fetchSongs(state))
-//       .then(() => { // return of async actions
-//         expect(store.getActions()).toEqual(expectedActions)
-//       })
-// })
+describe('fetchSongs action', () => {
+  afterEach(() => {
+    fetchMock.restore()
+  })
+
+  const state = {songTitle: "Lose Yourself"}
+
+  it('uses redux-promise to create an action object with type of "ADD_SONG" and a payload of songs when fetchSongs is dispatched', () => {
+    const mockSongQuery = `track.search?q_track=${state.songTitle}&page_size=10&page=1&s_track_rating=desc&apikey=523ebe747e1a258aaddd09f97f90cb70`
+
+    const url = BASE_URL + mockSongQuery
+
+    fetchMock.getOnce(url, {
+      data: {
+        message: {
+          header: {},
+          body: {
+            track_List: [
+              {track: {artist_name: "Eminem", track_name: "Lose Yourself"}},
+              {track: {artist_name: "Daft Punk feat. Pharrell Williams", track_name: "Lose Yourself to Dance"}}
+            ]
+          }
+        }
+      }
+    })
+
+
+
+    const expectedActions = [
+      {type: 'LOADING_SONG'},
+      { type: "ADD_SONG", payload: [
+        {track: {artist_name: "Eminem", track_name: "Lose Yourself"}}, {track: {artist_name: "Daft Punk feat. Pharrell Williams", track_name: "Lose Yourself to Dance"}}
+      ] }
+    ]
+
+    const store = mockStore({})
+    global.fetch = fetch
+
+    return store.dispatch(actions.fetchSongs(state))
+      .then(() => { // return of async actions
+        expect(store.getActions()).toContain({type: "LOADING_SONG"})
+      })
+  })
+})
 
 describe('fetchLyrics action', () => {
   afterEach(() => {
